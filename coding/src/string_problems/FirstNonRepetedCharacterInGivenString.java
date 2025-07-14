527a3d03c122f2185f393f31479e4ddd9acafad3
sahinsyed63 <sahin.syed63@gmaol.com>
2025-07-14 19:14:43 +0530

    solved-using java8,map

--- coding/src/string_problems/FirstNonRepetedCharacterInGivenString.java
+++ coding/src/string_problems/FirstNonRepetedCharacterInGivenString.java
@@ -1,5 +1,60 @@
 package string_problems;
 
-public class FirstNonRepetedCharacterInGivenString {
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+ public class FirstNonRepetedCharacterInGivenString {
+	public static void main(String args[]) {
+		String str = "hi shaheen";
+		
+		Map<Character,Integer> map = new LinkedHashMap<>();
+		
+		for(int i=0;i<str.length();i++) {
+			  if(!map.containsKey(str.charAt(i))) {
+				  map.put(str.charAt(i),1);
+			  }
+			  else {
+				  map.put(str.charAt(i),map.get(str.charAt(i))+1);
+			  }
+		}
+		
+	  Character ch =' ';
+		
+		for(Map.Entry<Character,Integer> entry:map.entrySet()) {
+			if(entry.getValue()==1) {
+				ch = entry.getKey();
+				break;
+				
+			}
+			
+		}
+		System.out.println("First Non Repeated Character :"+ ch);
+
+  /*  Map<Character,Long> map1 =
+    		          str.chars()
+    		          .mapToObj(c->(char)(c))
+    		          .collect(Collectors.groupingBy(Function.identity(),Collectors.counting()));
+		
+		System.out.println(map1);*/
+		
+		Optional<Character> map1 = str.chars().mapToObj(c->(char)(c))
+				                .collect(Collectors
+				               .groupingBy(Function.identity(),LinkedHashMap::new,Collectors.counting()))
+				                .entrySet()
+				                .stream()
+				                .filter(c->c.getValue()==1)
+				                .map(Map.Entry::getKey)
+				                .findFirst();
+		
+		
+		System.out.println(map1);
+		
+		
+		
+	}
 
 }
